6ae3c063660f0c15619e692bec963856370208de
xugang <371801080@qq.com>
2018-09-11 16:20:22 +0800

    nothing

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld'
+import Menu from '@/components/Menu'
 
-describe('HelloWorld.vue', () => {
+describe('Menu', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
+    const Constructor = Vue.extend(Menu)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.hello h1').textContent)
       .to.equal('Welcome to Your Vue.js App')
